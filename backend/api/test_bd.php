627afb9d301ebe4dce3d63f40b824ad3f336b3ba
Didi5i <christopher52banda@gmail.com>
2026-05-06 18:55:45 +0100

    Move backend files into backend folder

--- backend/api/test_bd.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: application/json');
-
-require_once __DIR__ . '/../includes/db.php';
-
-$stmt = $pdo->query("SELECT 1 AS ok");
-$result = $stmt->fetch(PDO::FETCH_ASSOC);
-
-echo json_encode($result);
